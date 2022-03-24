9b3bfe68c8fb4308670a3b372178f92c044b1100
shashi_kumar_p <shashi.papanna@innroad.com>
2022-03-24 22:42:52 +0530

    #comment added ApiOptions entity

---
+++ src/Vault.Client/Entities/ApiOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vault.Client.Entities
+{
+    public class ApiOptions
+    {
+        public string ApiPath { get; set; }
+
+        public HttpMethod HttpMethod { get; set; }
+
+        public object Data { get; set; }
+    }
+}
